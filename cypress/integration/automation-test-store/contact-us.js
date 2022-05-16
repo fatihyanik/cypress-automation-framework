dd38d3a97885c8ce436933b991b65d9516b0a450
fatihyanik <fatihyanik07@gmail.com>
2022-05-16 12:21:28 +0200

    added contact-us.js file in webdriver-uni folder

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -6,9 +6,8 @@ describe("Test Contact Us form via Automation Test Store", () => {
     //cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("Joe");
     cy.get("#ContactUsFrm_email").type("joe_blogs123@gmail.com");
-    cy.get("#ContactUsFrm_enquiry").type(
-      "Do you provide additional discount on bulk orders?"
-    );
-    cy.get(".col-md-6 > .btn").click();
+    cy.get("#ContactUsFrm_enquiry").type("Do you provide additional discount on bulk orders?");
+    cy.get("button[title='Submit']").click();
+    cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
   });
 });
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,8 +1,23 @@
-/// references types="cypress" />
+/// <reference types="cypress" />
 
-describe("Test Contact Us form via WebDriverUbi", ()=>{
-    it("Should be able to submit a successful mission via contact us form", () => {
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe");
-    }) 
+        cy.get('[name="last_name"]').type("blogs");
+        cy.get('[name="email"]').type("joe_blogs123@gmail.com")
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("Tom");
+        cy.get('[name="last_name"]').type("blogs");
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required');
+    });
 })
