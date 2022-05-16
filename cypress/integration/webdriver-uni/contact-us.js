424bf10e92b74dfe21a46b19dd9a3cb65ad4e2da
fatihyanik <fatihyanik07@gmail.com>
2022-05-16 12:35:27 +0200

    added inspect-item.js file

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+        cy.title().should('include', 'WebDriver | Contact Us');
+        cy.url().should('include', 'contactus');
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe");
         cy.get('[name="last_name"]').type("blogs");
