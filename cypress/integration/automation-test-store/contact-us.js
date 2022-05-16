6d61ce8980dfcb6f48251ca22a5db72df1048cb4
fatihyanik <fatihyanik07@gmail.com>
2022-05-16 13:34:30 +0200

    added variables and cypress command files

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -1,13 +1,18 @@
 /// <reference types="cypress" />
+
 describe("Test Contact Us form via Automation Test Store", () => {
-  it("Should be able to submit a successful submission via contact us form", () => {
-    cy.visit("https://www.automationteststore.com/");
-    cy.get(".info_links_footer > :nth-child(5) > a").click();
-    //cy.xpath("//a[contains(@href, 'contact')]").click();
-    cy.get("#ContactUsFrm_first_name").type("Joe");
-    cy.get("#ContactUsFrm_email").type("joe_blogs123@gmail.com");
-    cy.get("#ContactUsFrm_enquiry").type("Do you provide additional discount on bulk orders?");
-    cy.get("button[title='Submit']").click();
-    cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
-  });
-});
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        cy.get("a[href$='contact']").click().then(function(linkText){
+            cy.log("Clicked on link using text: " + linkText.text())
+        })
+        //cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+        cy.log("Test has completed!");
+    });
+})
---
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe("Verifying variables, cypress commands and jquery commands", () => {
+    it("Navigating to specific product pages", () => {
+        cy.visit("https://automationteststore.com/")
+        //The following will fail
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+        // makeupLink.click();
+        // skincareLink.click();
+
+        //The following will pass
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+        // makeupLink.click();
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+        // skincareLink.click();
+
+        //Recommended Approach
+        cy.get("a[href*='product/category&path=']").contains("Skincare").click()
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click()
+    });
+    it("Navigating to specific product pages", () => {
+        cy.visit("https://automationteststore.com/")
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click()
+
+        //Following code will fail
+        // const header = cy.get("h1 .maintext");
+        // cy.log(header.text())
+
+        cy.get("h1 .maintext").then(($headerText) => {
+            const headerText = $headerText.text()
+            cy.log("Found header text: " + headerText)
+            expect(headerText).is.eq('Makeup')
+        })
+    });
+
+})
